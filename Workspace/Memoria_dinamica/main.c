8e91e1b1603bb70b376718e9f81b607a9e29e5ac
alumno <alumno@lab6pc0>
2018-10-18 19:16:41 -0300

    01

--- Workspace/Memoria_dinamica/main.c
+++ Workspace/Memoria_dinamica/main.c
@@ -8,13 +8,16 @@ int funcion(void)//"funcion" representa a una direccion de memoria, funcion() ll
     return 7;
 }
 
+void funcionClientes(Cliente array[],int size)
+{
+
+}
+
 void funcionRecibeF(int (*pepe)(void))//Se arma la funcion dentro del argumento y se pasa su direccion
 {
     pepe();
     printf("puntero desde funcion: %p\n",pepe);
     printf("puntero desde funcion: %d\n",pepe());
-
-
 }
 
 int main()
