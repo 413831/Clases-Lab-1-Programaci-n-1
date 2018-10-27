4a4c4c4a3325d796ff0389481791101e843f4948
413831 <alepleins3pp@gmail.com>
2018-10-27 14:40:35 -0300

    Agregado Linked List

---
+++ Workspace/Linked_List/Employee.h
@@ -0,0 +1,34 @@
+#ifndef employee_H_INCLUDED
+#define employee_H_INCLUDED
+typedef struct
+{
+    int id;
+    char nombre[128];
+    int horasTrabajadas;
+    float sueldo;
+}Employee;
+
+Employee* employee_new();
+Employee* employee_newConParametros(char* id,char* nombre,char* horasTrabajadas,char* sueldo);
+int employee_EmployeeFromUser(void* pArrayListEmployee);
+
+int employee_input(char* mensaje,char* campo,int size, int (*pFunc)(char*));
+int employee_sort(void* thisA,void* thisB);
+int employee_delete();
+
+int employee_setId(Employee* this,char* id);
+int employee_getId(Employee* this,int* id);
+
+int employee_setNombre(Employee* this,char* nombre);
+int employee_getNombre(Employee* this,char* nombre);
+
+int employee_setHorasTrabajadas(Employee* this,char* horasTrabajadas);
+int employee_getHorasTrabajadas(Employee* this,int* horasTrabajadas);
+
+int employee_setSueldo(Employee* this,char* sueldo);
+int employee_getSueldo(Employee* this,float* sueldo);
+
+int employee_searchEmpty(Employee* array[], int size);
+int employee_show(Employee* this);
+
+#endif // employee_H_INCLUDED
