69d94275a9128bab4b062d16f53bb99d866f1473
alumno <alumno@LAB00PC00>
2018-11-13 20:02:06 -0300

    TP4 con test

--- test_tp4/LinkedList.c
+++ test_tp4/LinkedList.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-#include "../inc/LinkedList.h"
+#include "LinkedList.h"
 
 static Node* getNode(LinkedList* this, int nodeIndex);
 static int addNode(LinkedList* this, int nodeIndex,void* pElement);
---
+++ test_tp4/main.c
@@ -0,0 +1,10 @@
+#include <stdio_ext.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+#include "Controller.h"
+
+int main()
+{
+    controller_init();
+    return 0;
+}
