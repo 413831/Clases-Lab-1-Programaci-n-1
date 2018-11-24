11a7d814fffcb7e35c89ee6409a5d8ecf8ac8dba
413831 <alepleins3pp@gmail.com>
2018-11-23 22:07:26 -0300

    repositorio ordenado

--- Workspace/Prueba_Builder/parser.h
+++
@@ -1,4 +0,0 @@
-int parser_EmployeeFromText(FILE* pFile , LinkedList* pLinkedListEmployee);
-int parser_EmployeeFromBinary(FILE* pFile , LinkedList* pLinkedListEmployee);
-int parser_SaveToText(FILE* pFile , LinkedList* pLinkedListEmployee);
-int parser_SaveToBinary(FILE* pFile , LinkedList* pLinkedListEmployee);
